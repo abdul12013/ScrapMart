0b599f86b879e95d95795e533b3da96751a1ba1b
abdul12013 <140480419+abdul12013@users.noreply.github.com>
2025-08-03 22:56:35 +0530

    Update socket.js

--- frontEnd/src/socket.js
+++ frontEnd/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 
-export const socket = io("http://localhost:3000", {
+export const socket = io("https://scrapmart-backend.onrender.com", {
     transports: ['websocket'], // optional, can fix some polling issues
     withCredentials: true
   });
